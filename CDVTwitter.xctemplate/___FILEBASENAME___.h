08c5515cc49da04a4f78dd45ef39ad1a3ffc871e
Randy McMillan <randy.lee.mcmillan@gmail.com>
2013-01-08 00:02:28 -0500

    modified:   CDVTwitter.xctemplate/___FILEBASENAME___.h 	modified:   CDVTwitter.xctemplate/___FILEBASENAME___.m

--- CDVTwitter.xctemplate/___FILEBASENAME___.h
+++ CDVTwitter.xctemplate/___FILEBASENAME___.h
@@ -5,29 +5,32 @@
 //
 //  Original work by Antonelli Brian on 10/13/11.
 //
+//
+//  ___FILEBASENAME___
+//  CDVEmailComposer Template Created Jan 7 2013
+//  Copyright 2013 @RandyMcMillan
 
-    #import <Foundation/Foundation.h>
-    #import <Twitter/Twitter.h>
-    #import <Accounts/Accounts.h>
-    #import <Cordova/CDVPlugin.h>
+#import <Foundation/Foundation.h>
+#import <Twitter/Twitter.h>
+#import <Accounts/Accounts.h>
+#import <Cordova/CDVPlugin.h>
 
-@interface ___FILEBASENAME___ : CDVPlugin{
-}
+@interface ___FILEBASENAME___ : CDVPlugin {}
 
-- (void) isTwitterAvailable:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
-    
-- (void) isTwitterSetup:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
+- (void)isTwitterAvailable:(NSMutableArray *)arguments withDict:(NSMutableDictionary *)options;
 
-- (void) composeTweet:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
+- (void)isTwitterSetup:(NSMutableArray *)arguments withDict:(NSMutableDictionary *)options;
 
-- (void) getPublicTimeline:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
+- (void)composeTweet:(NSMutableArray *)arguments withDict:(NSMutableDictionary *)options;
 
-- (void) getTwitterUsername:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
+- (void)getPublicTimeline:(NSMutableArray *)arguments withDict:(NSMutableDictionary *)options;
 
-- (void) getMentions:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
+- (void)getTwitterUsername:(NSMutableArray *)arguments withDict:(NSMutableDictionary *)options;
 
-- (void) getTWRequest:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
+- (void)getMentions:(NSMutableArray *)arguments withDict:(NSMutableDictionary *)options;
 
-- (void) performCallbackOnMainThreadforJS:(NSString*)js;
+- (void)getTWRequest:(NSMutableArray *)arguments withDict:(NSMutableDictionary *)options;
+
+- (void)performCallbackOnMainThreadforJS:(NSString *)js;
 
 @end
